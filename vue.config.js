6a13694d0632f3c742c7c07a3f159b44c986eb36
unknown <1507402580@qq.com>
2022-09-29 10:48:30 +0800

    feat: 跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require("@vue/cli-service");
+const {
+  defineConfig
+} = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 项目打包的白屏问题
+  publicPath: "./",
+  devServer: {
+    // 端口号
+    port: "8888",
+    // 主机名
+    host: "localhost",
+    // 启动项目是否自动打开浏览器
+    open: false,
+    // 是否打开https
+    https: false,
+    // 跨域
+    proxy: {
+
+    }
+
+  },
+  lintOnSave: false
 });
